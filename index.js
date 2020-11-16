d1f7d83b233bbc06eb4f4f0f097fd0d9f7514b10
chirptotheeast <alyssa.easterly@yahoo.com>
2020-11-16 15:21:03 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+let drivers  = ['Milo', 'Otis', 'Garfield']
+// drivers.push('Ralph')
+// console.log(drivers)
+
+
+
+// defining function                 //arg passed in, can be vanilla generic variable
+function destructivelyAppendDriver(array){
+   return drivers.push('Ralph')
+}
+
+ function destructivelyPrependDriver(name){
+     return drivers.unshift('Bob')
+ }
+
+ function destructivelyRemoveLastDriver(){
+     return drivers.pop()
+
+ }
+
+ function destructivelyRemoveFirstDriver(){
+     return drivers.shift()
+ }
+
+ function appendDriver(name){
+    return [...drivers, name ]
+ }
+
+ function prependDriver(name){
+    return [name, ...drivers]
+ }
+
+ function removeLastDriver(){
+    return drivers.slice(0, drivers.length - 1)
+ }
+
+ function  removeFirstDriver(){
+     return drivers.slice(1)
+ }
